4f7f976da3d896eb60a09bf357df442164aed947
gamingexpx12 <gamingexpx12@gmail.com>
2017-03-05 16:51:41 +0100

    Added map generator from perlin noise

---
+++ Assets/Editor/MapGeneratorButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapGenerator))]
+public class MapGeneratorButton : Editor {
+
+	public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target; //Get reference to this map generator
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
+
+        if (GUILayout.Button ("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise
+{
+    public static float[,] GenerateNoiseMap(int mapWidthEnd, int mapHeightEnd, float scale) //Exclusive - Will have one less than provided
+    {
+        float[,] noiseMap = new float[mapWidthEnd, mapHeightEnd];
+
+        if (scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+        for (int y = 0; y < mapHeightEnd; y++)
+        {
+            for (int x = 0; x < mapWidthEnd; x++)
+            {
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap [x, y] = perlinValue;
+            }
+        }
+
+        return noiseMap;
+    }
+}
