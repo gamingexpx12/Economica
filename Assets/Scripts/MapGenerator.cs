6a40cbd4ac9b5a1f9ed14504c70cc55d63bdf86d
gamingexpx12 <gamingexpx12@gmail.com>
2017-03-05 17:33:12 +0100

    Better noise

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -8,11 +8,42 @@ public class MapGenerator : MonoBehaviour {
     public float noiseScale;
     public bool autoUpdate;
 
+    public int octaves = 1;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+    public int seed;
+    public Vector2 offset;
+
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap
+            (
+            mapWidth, mapHeight, seed, offset, 
+            noiseScale, octaves, persistance, lacunarity
+            );
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    private void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
     }
 }
--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -4,23 +4,77 @@ using UnityEngine;
 
 public static class Noise
 {
-    public static float[,] GenerateNoiseMap(int mapWidthEnd, int mapHeightEnd, float scale) //Exclusive - Will have one less than provided
+    public static float[,] GenerateNoiseMap
+        (
+        int mapWidthEnd, 
+        int mapHeightEnd, 
+        int seed,
+        Vector2 offset,
+        float scale, 
+        int octaves = 4, 
+        float persistance = 0.5f, 
+        float lacunarity = 2 
+        )
     {
         float[,] noiseMap = new float[mapWidthEnd, mapHeightEnd];
+
+        System.Random randomGen = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++)
+        {
+            float offsetX = randomGen.Next(-100000, 100000) + offset.x;
+            float offsetY = randomGen.Next(-100000, 100000) + offset.y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
 
         if (scale <= 0)
         {
             scale = 0.0001f;
         }
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        float halfWidth = mapWidthEnd / 2f;
+        float halfHeight = mapHeightEnd / 2f;
+
         for (int y = 0; y < mapHeightEnd; y++)
         {
             for (int x = 0; x < mapWidthEnd; x++)
             {
-                float sampleX = x / scale;
-                float sampleY = y / scale;
 
-                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
-                noiseMap [x, y] = perlinValue;
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for (int o = 0; o < octaves; o++)
+                {
+                    float sampleX = (x - halfWidth) / scale * frequency + octaveOffsets[o].x;
+                    float sampleY = (y - halfHeight) / scale * frequency + octaveOffsets[o].y;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+                if (noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }
+                else if (noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        for (int y = 0; y < mapHeightEnd; y++)
+        {
+            for (int x = 0; x < mapWidthEnd; x++)
+            {
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
             }
         }
 
