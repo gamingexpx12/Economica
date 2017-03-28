409cc9313e3fb7d0e828d5591cc1d3c1fa570055
gamingexpx12 <gamingexpx12@gmail.com>
2017-03-28 19:37:44 +0200

    Camera mover script

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Moves the camera along one axis.
+/// </summary>
+public class CameraMover : MonoBehaviour {
+    public enum axis { x, y, z};
+
+    public axis direction = axis.z;
+
+    [Range(0, 100)]
+    public float speed = 10f;
+
+	void Update () {
+        float x = direction == axis.x ? speed * Time.deltaTime : 0;
+        float y = direction == axis.y ? speed * Time.deltaTime : 0;
+        float z = direction == axis.z ? speed * Time.deltaTime : 0;
+        transform.position += new Vector3(x, y, z);
+	}
+}
