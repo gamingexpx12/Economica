bbb21c1f33e73c3a3c06486fbbc2011f6cf29bdb
gamingexpx12 <gamingexpx12@gmail.com>
2017-03-28 20:11:34 +0200

    Started on tools

---
+++ Assets/Scripts/Tools/RailTool.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailTool : MonoBehaviour {
+    /// <summary>
+    /// Terrain Layer
+    /// </summary>
+    int _layerMask = 1 << 8;
+
+	void FixedUpdate () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 50f, _layerMask))
+        {
+            transform.position = hit.point;
+        }
+	}
+}
