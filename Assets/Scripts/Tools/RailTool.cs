800384b1b94fd430affc5dc1154573decee58f1d
gamingexpx12 <gamingexpx12@gmail.com>
2017-03-30 20:42:29 +0200

    Testing

--- Assets/Scripts/Tools/RailTool.cs
+++ Assets/Scripts/Tools/RailTool.cs
@@ -3,17 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RailTool : MonoBehaviour {
+    public Vector3 cursorpositoion;
+    public GameObject cursorObject;
     /// <summary>
     /// Terrain Layer
     /// </summary>
     int _layerMask = 1 << 8;
+
+    private void Start()
+    {
+        Instantiate(cursorObject, transform);
+    }
+
 
 	void FixedUpdate () {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, 50f, _layerMask))
         {
-            transform.position = hit.point;
+
+            transform.position = _RoundVector(hit.point, 2);
         }
 	}
+
+    private Vector3 _RoundVector(Vector3 vector, int gridStep)
+    {
+        int x = RoundNum(Mathf.RoundToInt(vector.x), gridStep);
+        int y = RoundNum(Mathf.RoundToInt(vector.y), gridStep);
+        int z = RoundNum(Mathf.RoundToInt(vector.z), gridStep);
+
+        return new Vector3(x, y, z);
+
+    }
+
+    private float _RoundNum(float num, float step)
+    {
+        if (num >= 0)
+            return Mathf.Floor((num + step / 2) / step) * step;
+        else
+            return Mathf.Ceil((num - step / 2) / step) * step;
+    }
+    private int RoundNum(int num, int step)
+    {
+        if (num >= 0)
+            return ((num + (step / 2)) / step) * step;
+        else
+            return ((num - (step / 2)) / step) * step;
+    }
 }
