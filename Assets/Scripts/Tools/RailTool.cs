2693831f288eae3d12d77f2d4d1e88ce34a55275
gamingexpx12 <gamingexpx12@gmail.com>
2017-03-31 19:43:52 +0200

    Snapping

--- Assets/Scripts/Tools/RailTool.cs
+++ Assets/Scripts/Tools/RailTool.cs
@@ -47,7 +47,7 @@ public class RailTool : MonoBehaviour {
         if (Physics.Raycast(ray, out hit, 50f, _layerMask))
         {
 
-            cursorPosition = _RoundVector(hit.point, 2);
+            cursorPosition = _SnapToGrid(hit.point, 2);
         }
 	}
 
@@ -74,5 +74,17 @@ public class RailTool : MonoBehaviour {
             return ((num + (step / 2)) / step) * step;
         else
             return ((num - (step / 2)) / step) * step;
+    }
+
+    private Vector3 _SnapToGrid(Vector3 location, int snap)
+    {
+        int x = snap * Mathf.RoundToInt(location.x / snap);
+        int y = snap * Mathf.RoundToInt(location.y / snap);
+        int z = snap * Mathf.RoundToInt(location.z / snap);
+
+        //x = x % snap ;
+        //print(x / snap);
+        //x = snap* Mathf.Round(location.x / snap);
+        return new Vector3(x, y, z);
     }
 }
