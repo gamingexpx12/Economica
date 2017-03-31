d45bb71e8d53487b8a9c5e16af5cd783753817ea
gamingexpx12 <gamingexpx12@gmail.com>
2017-03-31 19:45:50 +0200

    Clean up

--- Assets/Scripts/Tools/RailTool.cs
+++ Assets/Scripts/Tools/RailTool.cs
@@ -8,7 +8,6 @@ public class RailTool : MonoBehaviour {
     public Material ghostMaterial;
 
     public string UseButton;
-    bool _useKeyHeld;
     /// <summary>
     /// Terrain Layer
     /// </summary>
@@ -50,31 +49,6 @@ public class RailTool : MonoBehaviour {
             cursorPosition = _SnapToGrid(hit.point, 2);
         }
 	}
-
-    private Vector3 _RoundVector(Vector3 vector, int gridStep)
-    {
-        int x = RoundNum(Mathf.RoundToInt(vector.x), gridStep);
-        int y = RoundNum(Mathf.RoundToInt(vector.y), gridStep);
-        int z = RoundNum(Mathf.RoundToInt(vector.z), gridStep);
-
-        return new Vector3(x, y, z);
-
-    }
-
-    private float _RoundNum(float num, float step)
-    {
-        if (num >= 0)
-            return Mathf.Floor((num + step / 2) / step) * step;
-        else
-            return Mathf.Ceil((num - step / 2) / step) * step;
-    }
-    private int RoundNum(int num, int step)
-    {
-        if (num >= 0)
-            return ((num + (step / 2)) / step) * step;
-        else
-            return ((num - (step / 2)) / step) * step;
-    }
 
     private Vector3 _SnapToGrid(Vector3 location, int snap)
     {
