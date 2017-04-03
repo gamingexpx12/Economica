fd751c238d439afb88ca7e477e82d0e243995a9d
gamingexpx12 <gamingexpx12@gmail.com>
2017-04-03 10:55:24 +0200

    Update RoadTool.cs

--- Assets/Scripts/Tools/RoadTool.cs
+++ Assets/Scripts/Tools/RoadTool.cs
@@ -1,16 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoadTool : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class RoadTool 
+{
+	public GameObject[] tools;
 }
