dd8def201430944a504234c5accbf02bb1d67428
gamingexpx12 <gamingexpx12@gmail.com>
2017-04-05 17:07:19 +0200

    Added seperate ghosting component

---
+++ Assets/Scripts/Tools/Ghosting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghosting : MonoBehaviour {
+
+    Vector3 cursorPosition;
+    GameObject GhostModel;
+    public Material ghostMaterial;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Ghost(GameObject modelPrefab, Vector3[] positions)
+    {
+
+    }
+
+    public void RemoveGhosts()
+    {
+
+    }
+}
