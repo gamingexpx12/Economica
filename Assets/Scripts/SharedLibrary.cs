c65f62a85beb310b1d7f2b40c05e9be3fa19817a
gamingexpx12 <gamingexpx12@gmail.com>
2017-04-06 19:08:28 +0200

    Made a shared library for functions.

---
+++ Assets/Scripts/SharedLibrary.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SharedLibrary
+{
+    /// <summary>
+    /// Compares x and z values of a vector. Z is ignored.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static bool VectorLocationEqual(Vector3 a, Vector3 b)
+    {
+        bool xval = a.x == b.x;
+        bool zval = a.z == b.z;
+        if (xval & zval)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
