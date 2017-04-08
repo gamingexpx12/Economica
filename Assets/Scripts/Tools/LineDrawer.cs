6f2e6d23cb84ad445f6c2572f0848750e18c5148
gamingexpx12 <gamingexpx12@gmail.com>
2017-04-08 19:22:37 +0200

    Replaced conditonals checking for begin == end with an object. It does create excessive amounts of code though.

--- Assets/Scripts/Tools/LineDrawer.cs
+++ Assets/Scripts/Tools/LineDrawer.cs
@@ -42,6 +42,9 @@ public class LineDrawer: MonoBehaviour
     Vector3 _end;
     Quaternion _direction;
     public Vector3 _diff;
+    LocationBasedFunctions sameFuncs;
+    LocationBasedFunctions diffFuncs;
+    LocationBasedFunctions locationFuncs;
 
     public void DrawLine()
     {
@@ -50,18 +53,25 @@ public class LineDrawer: MonoBehaviour
     }
     private void Start()
     {
+        sameFuncs = new SameLocation(this);
+        diffFuncs = new DifferentLocation(this);
+        locationFuncs = sameFuncs;
+
         _ghoster = GetComponent<Ghosting>();
     }
 
     private void Update()
     {
+        //Pick diffrent functions if were only drawing one instance.
+        locationFuncs = SharedLibrary.VectorLocationEqual(_begin, _end) ? sameFuncs : diffFuncs;
+
         _diff = _end - _begin;
         int furthestAxis = Mathf.RoundToInt(Mathf.Max(Mathf.Abs(_diff.x), Mathf.Abs(_diff.z)));
         distanceTile = furthestAxis / grid;
         distanceReal = furthestAxis;
         debugPosition = _begin + _SnapToCardinal(_end, distanceReal);
 
-        _SetInstances(_begin, debugPosition);
+        locationFuncs._SetInstances(_begin, debugPosition);
         numInstances = _ghosts.Length;
         _direction = _GetDirection(_begin, debugPosition);
         _ghoster.Ghost(_model, _direction, _ghosts);
@@ -130,5 +140,62 @@ public class LineDrawer: MonoBehaviour
     {
         Vector3 cardinal = SharedLibrary.CardinalDirection(vector);
         return cardinal * distance;
+    }
+
+    private abstract class LocationBasedFunctions
+    {
+        public LineDrawer parent;
+        public LocationBasedFunctions(LineDrawer parent)
+        {
+            this.parent = parent;
+        }
+        public abstract void _SetInstances(Vector3 begin, Vector3 end);
+        public abstract Quaternion _GetDirection(Vector3 begin, Vector3 end);
+    }
+
+    private class SameLocation : LocationBasedFunctions
+    {
+        public SameLocation(LineDrawer parent) : base(parent)
+        {
+        }
+
+        public override Quaternion _GetDirection(Vector3 begin, Vector3 end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void _SetInstances(Vector3 begin, Vector3 end)
+        {
+            parent._ghosts = new Vector3[1];
+            parent._ghosts[0] = begin;
+            return;
+        }
+    }
+
+    private class DifferentLocation : LocationBasedFunctions
+    {
+        public DifferentLocation(LineDrawer parent) : base(parent)
+        {
+        }
+
+        public override Quaternion _GetDirection(Vector3 begin, Vector3 end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void _SetInstances(Vector3 begin, Vector3 end)
+        {
+            int dist = parent.distanceTile + 1;
+            parent._ghosts = new Vector3[dist];
+
+            for (int tile = 0; tile < dist; tile++)
+            {
+                float tilePosF = (float)tile / (float)parent.distanceTile;
+                Vector3 tilePos = Vector3.Lerp(begin, end, tilePosF);
+                parent._ghosts[tile] = tilePos;
+
+
+            }
+        }
     }
 }
