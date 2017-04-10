ce133d6de3674a2e40cc53c6f8623b0cbe7f4038
gamingexpx12 <gamingexpx12@gmail.com>
2017-04-10 11:21:55 +0200

    Fully implemented SingleTileDrawer.

--- Assets/Scripts/Tools/SingleTileDrawer.cs
+++ Assets/Scripts/Tools/SingleTileDrawer.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class SingleTileDrawer : MonoBehaviour {
 
-	public LineData MakeSingleTile(Vector3 position)
+	public LineData MakeSingleTile(Vector3 worldPosition, Vector3 inTilePosition)
     {
         LineData sqareData = new LineData();
 
+        var direction = new Quaternion();
+        var cardinal = SharedLibrary.CardinalDirection(inTilePosition);
+        direction.SetLookRotation(cardinal);
+        sqareData.direction = direction;
+
+        var instance = new Vector3[1];
+        instance[0] = worldPosition;
+        sqareData.instances = instance;
         return sqareData;
     }
 }
