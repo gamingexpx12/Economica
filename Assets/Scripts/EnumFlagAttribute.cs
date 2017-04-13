330babd15e53ae785e8a20b724e940055da00f04
gamingexpx12 <gamingexpx12@gmail.com>
2017-04-13 17:16:00 +0200

    Added documentation and example.

--- Assets/Scripts/EnumFlagAttribute.cs
+++ Assets/Scripts/EnumFlagAttribute.cs
@@ -1,5 +1,17 @@
 ﻿using UnityEngine;
-
+/// <summary>
+/// Use this to make an enum into a collection of flags.
+/// 
+/// Example enum:
+///     [Flags] 
+///     Public Enum CanBeCompletedBy 
+///     {
+///         Maya = 1,
+///         Phoenix = 2,
+///         Apollo = 4
+///     }
+/// 
+/// </summary>
 class EnumFlagAttribute : PropertyAttribute
 {
     public string enumName;
