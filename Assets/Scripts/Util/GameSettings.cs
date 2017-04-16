5800e468963c307233d773aa1286ed26a69c430b
gamingexpx12 <gamingexpx12@gmail.com>
2017-04-16 20:48:25 +0200

    Added GameSettings s.object

---
+++ Assets/Scripts/Util/GameSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "GameSettings", menuName = "GameSettings")]
+public class GameSettings : ScriptableObject {
+
+    public int GridSize;
+    public float ScreenToWorldRaycastLength;
+
+}
