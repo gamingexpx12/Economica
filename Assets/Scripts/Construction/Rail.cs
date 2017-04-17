a9e28468f992ccc43837ed6ed26ed1e19b8a2f52
gamingexpx12 <gamingexpx12@gmail.com>
2017-04-17 10:48:52 +0200

    Added rail component

---
+++ Assets/Scripts/Construction/Rail.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rail : MonoBehaviour {
+    [EnumFlag]
+    TrackDirection track;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+}
+
+[System.Flags]
+public enum TrackDirection
+{
+    NS = 1,
+    EW = 2,
+    NW = 4,
+    NE = 8,
+    SW = 16,
+    SE = 32
+}
