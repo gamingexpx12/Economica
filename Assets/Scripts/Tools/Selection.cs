c10aa7a2fafda3a6f49c0a223f205a9861b37903
gamingexpx12 <gamingexpx12@gmail.com>
2017-04-19 16:01:18 +0200

    Moved singleTile into the selection class.

--- Assets/Scripts/Tools/Selection.cs
+++ Assets/Scripts/Tools/Selection.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Selection : MonoBehaviour {
+public static class Selection {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static LineData SelectTile(Vector3 worldPosition, Vector3 inTilePosition)
+    {
+        LineData sqareData = new LineData();
+
+        var direction = new Quaternion();
+        var cardinal = SharedLibrary.CardinalDirection(inTilePosition, SharedLibrary.North);
+        direction.SetLookRotation(cardinal);
+        sqareData.direction = direction;
+        sqareData.trackDirection = SharedLibrary.CardinalToTrackDirection(cardinal);
+
+        var instance = new Vector3[1];
+        instance[0] = worldPosition;
+        sqareData.instances = instance;
+        return sqareData;
+    }
+
+    public static LineData SelectLine(Vector3 beginPosition, Vector3 endPosition)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public static LineData SelectArea(Vector3 beginPosition, Vector3 endPosition)
+    {
+        throw new System.NotImplementedException();
+    }
 }
