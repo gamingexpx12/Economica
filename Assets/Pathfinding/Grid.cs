e8b496f3bf14b5d9512c4ef092884e06ef4ff74a
gamingexpx12 <gamingexpx12@gmail.com>
2017-04-27 07:49:51 +0200

    extracted add node code to its own method.

--- Assets/Pathfinding/Grid.cs
+++ Assets/Pathfinding/Grid.cs
@@ -44,11 +44,16 @@ public class Grid : MonoBehaviour
         {
             for (int y = 0; y < gridSizeY; y++)
             {
-                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
-                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius - 0.1f, unwalkableMask));
-                grid[x, y] = new Node(walkable, worldPoint, x, y);
+                AddNodeToGrid(worldBottomLeft, x, y);
             }
         }
+    }
+
+    private void AddNodeToGrid(Vector3 worldBottomLeft, int x, int y)
+    {
+        Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+        bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius - 0.1f, unwalkableMask));
+        grid[x, y] = new Node(walkable, worldPoint, x, y);
     }
 
     public List<Node> GetNeighbours(Node node)
