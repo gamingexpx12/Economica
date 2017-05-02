ab34856ad3853adaf38c741a93705355be17469e
gamingexpx12 <gamingexpx12@gmail.com>
2017-05-02 21:39:16 +0200

    Transfer from Super Trains 3D

---
+++ Assets/Pathfinding/Unit.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 5.0F;
+    public Vector3[] path;
+    int targetIndex;
+
+    void Start()
+    {
+        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+    }
+
+    public void OnPathFound(Vector3[] newpath, bool pathSuccessful)
+    {
+        if (pathSuccessful)
+        {
+            path = newpath;
+            StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+    }
+
+    public IEnumerator FollowPath()
+    {
+        Vector3 currentWaypoint = path[0];
+
+        while (true)
+        {
+            if (transform.position == currentWaypoint)
+            {
+                targetIndex++;
+                if (targetIndex >= path.Length)
+                {
+                    targetIndex = 0;
+                    path = new Vector3[0];
+                    yield break;
+                }
+                currentWaypoint = path[targetIndex];
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (path != null)
+        {
+            Gizmos.color = Color.black;
+            for (int i = targetIndex; i < path.Length; i++)
+            {
+                Gizmos.DrawWireCube(path[i], Vector3.one);
+
+                if (i == targetIndex)
+                {
+                    Gizmos.DrawLine(transform.position, path[i]);
+                }
+                else
+                {
+                    Gizmos.DrawLine(path[i - 1], path[i]);
+                }
+            }
+        }
+    }
+}
