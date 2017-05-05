085840b426c213a329a6e0bdfc67a26b745fa895
gamingexpx12 <gamingexpx12@gmail.com>
2017-05-05 18:45:24 +0200

    Implemented pathDebugger.

--- Assets/Pathfinding/PathDebugger.cs
+++ Assets/Pathfinding/PathDebugger.cs
@@ -6,14 +6,17 @@ public class PathDebugger : MonoBehaviour {
 
     public int x;
     public int y;
-
+    public Node _node;
+    Grid _grid;
 	// Use this for initialization
 	void Start () {
-		
+        _grid = GameObject.FindGameObjectWithTag("Pathing").GetComponent<Grid>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        _node = _grid.NodeFromWorldPoint(transform.position);
+        x = _node.gridX;
+        y = _node.gridY;
 	}
 }
